61914324ced1cdc94612a3b45d066ac5e106c93f
Gogs <gogs@fake.local>
2018-04-24 19:54:35 -0400

    update

--- service.php
+++ service.php
@@ -1,18 +1,29 @@
 <?php
 
+/**
+ * Apretaste Google Service
+ *
+ * @author kumahacker
+ * @version 2.0
+ */
+
 class Google extends Service
 {
+	const ENGINE_GOOGLE = 1;
+	const ENGINE_DUCKDUCKGO = 2;
+	const ENGINE_YANDEX = 3;
+	const ENGINE_FAROO = 4;
+
 	/**
 	 * Perform a Google search
 	 *
 	 * @param Request
 	 * @return Response
 	 */
-	public function _main (Request $request)
+	public function _main(Request $request)
 	{
 		// do not allow a blank search
-		if(empty($request->query))
-		{
+		if (empty($request->query)) {
 			$response = new Response();
 			$response->setCache();
 			$response->setResponseSubject("Que desea buscar en Google?");
@@ -20,26 +31,8 @@ class Google extends Service
 			return $response;
 		}
 
-		// include lib and create object
-		require_once "{$this->pathToService}/lib/CustomSearch.php";
-		$cs = new Fogg\Google\CustomSearch\CustomSearch();
-
-		// perform a simple search
-		$gresults = null;
-		try {
-			$gresults = $cs->simpleSearch($request->query);
-		} catch(Exception $e){}
-
-		// clean if exist results
-		$results = array();
-		if (isset($gresults->items))
-		foreach ($gresults->items as $gresult){
-			$results[] =  array(
-				"title" => $this->utils->removeTildes($gresult->htmlTitle),
-				"url" => $gresult->link,
-				"note" => $this->utils->removeTildes($gresult->htmlSnippet)
-			);
-		}
+		// search
+		$results = $this->search($request->query);
 
 		// load empty template if no results
 		if (empty($results)) $template = "noresults.tpl";
@@ -57,5 +50,149 @@ class Google extends Service
 		$response->setResponseSubject("Google: " . $request->query);
 		$response->createFromTemplate($template, $responseContent);
 		return $response;
+	}
+
+	/**
+	 * Generic searcher
+	 *
+	 * @author kumahacker
+	 * @param string $q
+	 * @param integer $engine
+	 *
+	 * @return array
+	 */
+	private function search($q, $engine = Google::ENGINE_GOOGLE)
+	{
+		$results = [];
+		$di = \Phalcon\DI\FactoryDefault::getDefault();
+		$wwwroot = $di->get('path')['root'];
+		$cacheFile = "$wwwroot/temp/google/" . md5($q);
+
+		// check the cache
+		if (file_exists($cacheFile) && time() - filemtime($cacheFile) > 100000) {
+			$content = file_get_contents($cacheFile);
+			$results = json_decode($content);
+			return $results;
+		}
+
+		switch ($engine) {
+			case Google::ENGINE_GOOGLE:
+
+				// include lib and create object
+				require_once "{$this->pathToService}/lib/CustomSearch.php";
+				$cs = new Fogg\Google\CustomSearch\CustomSearch();
+
+				// perform a simple search
+				$gresults = null;
+				try {
+					$gresults = $cs->simpleSearch($q);
+				} catch (Exception $e) {
+				}
+
+				// clean if exist results
+				if (isset($gresults->items))
+					foreach ($gresults->items as $gresult) {
+						$results[] = [
+							"title" => $this->utils->removeTildes($gresult->htmlTitle),
+							"url" => $gresult->link,
+							"note" => $this->utils->removeTildes($gresult->htmlSnippet)
+						];
+					}
+
+				break;
+
+			case Google::ENGINE_DUCKDUCKGO:
+				$content = $this->getRemoteContent("https://api.duckduckgo.com/?q=" . urlencode($q) . "&format=json");
+				$content = json_decode($content);
+				if (isset($content->results))
+					foreach ($content->results as $v) {
+						if (is_object($v)) $v = get_object_vars($v);
+						$v['title'] = '';
+						$v['url'] = '';
+						$v['note'] = '';
+						$results[] = $v;
+					}
+				break;
+
+			case Google::ENGINE_YANDEX:
+				break;
+
+			case Google::ENGINE_FAROO:
+
+				$config = $this->config['search-api-faroo'];
+
+				// http://www.faroo.com/api?q=cuba&start=1&length=10&l=en&src=web&i=false&f=json&key=G2POOpVSD35690JspEW8SxnI@XI_
+				$url = $config['base_url'] . '?' . (empty($query) ? '' : 'q=' . urlencode("$query"));
+				$url .= "&start=1";
+				$url .= "&length=" . $config['results_length'];
+				$url .= "&l=es";
+				$url .= "&src=web";
+				$url .= "&i=false&f=json&key=" . $config['key'];
+
+				$content = $this->getRemoteContent($url);
+				$result = @json_decode($content);
+
+				if (isset($result->results)) if (is_array($result->results)) {
+					foreach ($result->results as $k => $v) {
+						if (is_object($v)) $v = get_object_vars($v);
+						$v['date'] = date("d/m/Y", $v->date);
+						$v['title'] = '';
+						$v['url'] = '';
+						$v['note'] = '';
+						$result->results[$k] = $v;
+					}
+					$results = $result->results;
+				}
+				break;
+		}
+
+		file_put_contents($cacheFile, json_encode($results));
+
+		return $results;
+	}
+
+	/**
+	 * Return a remote content
+	 *
+	 * @param $url
+	 * @param array $info
+	 * @return mixed
+	 */
+	private function getRemoteContent($url, &$info = [])
+	{
+		$url = str_replace("//", "/", $url);
+		$url = str_replace("http:/", "http://", $url);
+		$url = str_replace("https:/", "https://", $url);
+
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_URL, $url);
+
+		$default_headers = [
+			"Cache-Control" => "max-age=0",
+			"Origin" => "{$url}",
+			"User-Agent" => "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36",
+			"Content-Type" => "application/x-www-form-urlencoded"
+		];
+
+		$hhs = [];
+		foreach ($default_headers as $key => $val)
+			$hhs[] = "$key: $val";
+
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $hhs);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+		$html = curl_exec($ch);
+
+		$info = curl_getinfo($ch);
+
+		if ($info['http_code'] == 301)
+			if (isset($info['redirect_url']) && $info['redirect_url'] != $url)
+				return $this->getUrl($info['redirect_url'], $info);
+
+		curl_close($ch);
+
+		return $html;
 	}
 }
